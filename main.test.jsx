a52a7864eea1c8642764902f572a46360d682cf0
StefaniaSerbanoiu <127020225+StefaniaSerbanoiu@users.noreply.github.com>
2024-03-20 14:01:14 +0200

    first assignment

---
+++ main.test.jsx
@@ -0,0 +1,63 @@
+import { it, expect, describe } from 'vitest';
+import { render, screen } from '@testing-library/react'
+import Home from '../src/Home';
+import React from 'react';
+import Update from '../src/Update';
+import Create from '../src/Create';
+import Read from '../src/Read';
+import { BrowserRouter } from 'react-router-dom';
+
+
+
+
+describe('Home Component', () => {
+  it('renders without crashing', () => {
+    console.log("111111111111111111111111111111111");
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    console.log("222222222222222222222222222222222");
+    const headingElement = screen.getByText('List of products');
+    expect(headingElement).not.toBeNull();
+  });
+});
+
+
+describe('Update Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <BrowserRouter>
+        <Update />
+      </BrowserRouter>
+    );
+    const headingElement = screen.getByText('Update product info');
+    expect(headingElement).not.toBeNull();
+  });
+});
+
+
+describe('Create Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <BrowserRouter>
+        <Create />
+      </BrowserRouter>
+    );
+    const headingElement = screen.getByText('Add');
+    expect(headingElement).not.toBeNull();
+  });
+});
+
+describe('View Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <BrowserRouter>
+        <Read />
+      </BrowserRouter>
+    );
+    const headingElement = screen.getByText('Details');
+    expect(headingElement).not.toBeNull();
+  });
+});
