dfc6b01afc1f91becc7f81f7f698f847f114afec
StefaniaSerbanoiu <127020225+StefaniaSerbanoiu@users.noreply.github.com>
2024-04-17 18:56:45 +0300

    Delete duplicated src directory

--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Shoe (
-                      shoe_id INT AUTO_INCREMENT PRIMARY KEY,
-                      product_name VARCHAR(255) NOT NULL,
-                      size INT NOT NULL,
-                      price INT NOT NULL
-);
